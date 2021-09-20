a71d2d5027ab7eca22935a4f80b830e1701fdd78
Bruna Neto Bitencourt <brunaneto.b@gmail.com>
2021-09-20 11:28:51 -0300

    Criação da rota GET grupo

--- scripts/server/server.js
+++ scripts/server/server.js
@@ -65,3 +65,9 @@ app.post("/grupo", (req, res) => {
     groupControler.insertGrupo(grupo);
   }
 });
+
+app.get("/grupo/:id", async function (req, res) {
+  console.log("chegou na rota GET GRUPO");
+  const grupo = await groupControler.selectGrupo(req.params.id);
+  res.json(grupo);
+});
